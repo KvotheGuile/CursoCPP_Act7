9a35eca4627424116bc72638e7953c852479a1cf
KvotheGuile <kvothejoestar@gmail.com>
2024-11-17 16:59:06 -0700

    Nuevo caso de prueba

--- main.cpp
+++ main.cpp
@@ -11,8 +11,10 @@ int main()
 
     // Triangle 2
     Triangle triangle2(Point(10, 10), Point(10, 13), Point(14, 10));
+    Triangle triangle3(Point(5, 5), Point(5, 5), Point(5, 5));
     
     cout<<"Triangle 1 "; triangleDefault.PrintInfo();
     cout<<"Triangle 2 "; triangle2.PrintInfo();
+    cout<<"Triangle 3 "; triangle3.PrintInfo();
     return 0;
 }
