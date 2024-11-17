ab8e2ba17a88b33ac7aabef1f92637f78796e3da
KvotheGuile <kvothejoestar@gmail.com>
2024-11-17 10:59:49 -0700

    Another triangle added

--- main.cpp
+++ main.cpp
@@ -8,7 +8,11 @@ int main()
 {
     // Triangle 1
     Triangle triangleDefault;
+
+    // Triangle 2
+    Triangle triangle2(Point(10, 10), Point(10, 13), Point(14, 10));
     
     cout<<"Triangle 1 "; triangleDefault.PrintInfo();
+    cout<<"Triangle 2 "; triangle2.PrintInfo();
     return 0;
 }
